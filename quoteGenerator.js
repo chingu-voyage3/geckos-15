414a60906059fbea002c027be7142d51b7580428
dpbrinkm <33791923+dpbrinkm@users.noreply.github.com>
2017-12-04 23:32:52 +0100

    very beginning
    here are some basics. 
    
    so far i still need to work on for this version:
    
    adding the on click function and passing through the submitted value to compare if answer is correct
    populate the movie list
    make the code compatible with JSON (not really sure how)
    add the functionality to the score keeper element // connect the counter with the score id
    
    for other versions:
    
    partial credit for almost guessing the correct answer
    making it so the computer remembers which movies they saw already so you don't get a repeat
    fade in function
    css and styling it
    link with pomodor clock 
    ask if they want to play after the clock finishes

---
+++ quoteGenerator.js
@@ -0,0 +1,21 @@
+
+
+
+let counter = "";
+let rndObject = movies[Math.floor(Math.random()*movies.length)];
+
+
+function generateQuote(){
+	//shows quote from the json objects 
+	let newQuote = rndObject.quote;
+	document.getElementById('quoteHeader').innerHTML = newQuote; 
+};
+
+
+function answerCheck(userAnswer){
+	if (userAnswer == rndObject.movie){
+		counter += 2000;
+		alert ("You did it!");
+	}
+	else return alert("Nice try");
+}
