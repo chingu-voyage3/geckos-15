11d26c6e326bd4e8c17745bac0311b354ebede8b
dpbrinkm <dpbrinkm@gmail.com>
2017-12-06 23:01:25 +0100

    working
    got the call back functions working and am now able to see the value of whats in the text box and if its the same as movie title

--- moviequotequiz/assets/quoteGenerator.js
+++ moviequotequiz/assets/quoteGenerator.js
@@ -2,9 +2,11 @@
 //document.getElementById("quoteHeader").innerHTML = movies[0].quote;
 
 
-let rndObject = movies[Math.floor(Math.random()* movies.length)];
-let movieTitle = rndObject.movie;
-let movieQuote = rndObject.quote;
+let rndNumber = [Math.floor(Math.random()* movies.length)];
+let movieTitle = movies[rndNumber].movie;
+let movieQuote = movies[rndNumber].quote;
+console.log(movieQuote);
+console.log(movieTitle);
 
 function generateQuote(){
 	//shows quote from the json objects 
@@ -12,16 +14,34 @@ function generateQuote(){
 };
 generateQuote();
 
-var userAnswer = document.getElementById('userAnswerText').innerHTML;
+/*
 let counter = 0; 
 
-
-function answerCheck(){
+answerCheck.addEventListener('click', userAnswerCheck());
+function userAnswerCheck(){
+	var userAnswer = document.getElementById('userAnswerText').innerHTML;
+	console.log(userAnswerText)
 	if (userAnswer == movieTitle){
 		counter += 2000;
 		alert ("You did it!");
 	}
-	else return alert("Close, but no cigar");
-	console.log(userAnswer);
+	else alert("Close, but no cigar");
+	//console.log(userAnswer);
 }
+*/
+let counter = 0;
+const answerCheck = document.getElementById('answerButton');
+
+answerCheck.addEventListener('click', userAnswerCheck);
+
+function userAnswerCheck(){
+   const userAnswerText = document.getElementById('userAnswerText');
+   console.log(userAnswerText.value)
+   if (userAnswerText.value == movieTitle){
+       counter += 2000;
+       alert ("You did it!");
+   }
+   else alert("Close, but no cigar");
+}
+
 
