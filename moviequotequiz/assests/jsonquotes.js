3fb8509d21b5dff00550785dc176e61aa057079b
dpbrinkm <dpbrinkm@gmail.com>
2017-12-06 16:56:39 +0100

    better organized

---
+++ moviequotequiz/assests/jsonquotes.js
@@ -0,0 +1,69 @@
+
+
+{ var movies = [
+{ 	
+	"movie": "The Big Lebawski";
+	"quote": "I don't roll on the sabbath";
+},
+{
+	"movie": "Superbad";
+	"quote": "Sorry the Cohen brothers dont direct my porn";
+
+},
+{
+	"movie": "Lord of the Rings";
+	"quote": "One ring to rule them all";
+},
+{
+	"movie": "Animal House";
+	"quote": "Fat, drunk, and stupid is no way to go through life, son.";
+
+},
+{
+	"movie": "Zoolander";
+	"quote": "What is this?? A school for ants??";
+
+},
+{
+	"movie": "Blues Brothers";
+	"quote": "They’re not gonna catch us. We’re on a mission from God.";
+
+},
+{
+	"movie": "Dr. Stangelove";
+	"quote": "Gentlemen, you can’t fight in here! this is the War Room.";
+
+},
+{
+	"movie": "Dodgeball";
+	"quote": "Oh, I don’t think I’m a lot dumber than you think that I thought that I thought I was once.";
+
+},
+{
+	"movie": "Home Alone";
+	"quote": "I believe you but my tommy gun don't";
+
+},
+{
+	"movie": "Blade Runner";
+	"quote": "I've seen things you people wouldn't believe. Attack ships on fire off the shoulder of Orion. I watched C-beams glitter in the dark near the Tannhäuser Gate. All those moments will be lost in time, like tears in rain. Time to die";
+
+},
+{
+	"movie": "They Live";
+	"quote": "I have come here to chew bubblegum and kick ass... and I'm all out of bubblegum";
+
+},
+{
+	"movie": "Ghostbusters";
+	"quote": "when someone asks you if you are a god, you say yes ...";
+
+},
+{
+	"movie": "Christmas Vacation";
+	"quote": "The little lights aren’t twinkling Clark";
+
+}
+]}
+
+
