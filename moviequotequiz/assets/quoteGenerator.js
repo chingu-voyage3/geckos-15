b4ec6fd449cd9516524affa9fcb81db2fa9343de
dpbrinkm <dpbrinkm@gmail.com>
2017-12-06 19:15:14 +0100

    updated with onclick
    not able to get the userAnswer variable to return anything other than an empty string

--- moviequotequiz/assets/quoteGenerator.js
+++ moviequotequiz/assets/quoteGenerator.js
@@ -1,23 +1,27 @@
 
-document.getElementById("quoteHeader").innerHTML = movies[0].quote;
+//document.getElementById("quoteHeader").innerHTML = movies[0].quote;
 
-/*
-let counter = "";
-let rndObject = movies[Math.floor(Math.random()*movies.length)];
 
+let rndObject = movies[Math.floor(Math.random()* movies.length)];
+let movieTitle = rndObject.movie;
+let movieQuote = rndObject.quote;
 
 function generateQuote(){
 	//shows quote from the json objects 
-	let newQuote = rndObject.quote;
-	document.getElementById('quoteHeader').innerHTML = newQuote; 
+	document.getElementById('quoteHeader').innerHTML = movieQuote; 
 };
-function generateQuote();
+generateQuote();
 
-function answerCheck(userAnswer){
-	if (userAnswer == rndObject.movie){
+var userAnswer = document.getElementById('userAnswerText').innerHTML;
+let counter = 0; 
+
+
+function answerCheck(){
+	if (userAnswer == movieTitle){
 		counter += 2000;
 		alert ("You did it!");
 	}
-	else return alert("Nice try");
+	else return alert("Close, but no cigar");
+	console.log(userAnswer);
 }
-*/
+
